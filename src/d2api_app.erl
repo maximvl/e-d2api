20d50e306ec761a8222006cc259c4ba674be7952
Maxim V <loz.accs@gmail.com>
2013-06-10 21:28:03 +0000

    added steam api key file to app settings

--- src/d2api_app.erl
+++ src/d2api_app.erl
@@ -11,10 +11,18 @@
 
 start() ->
   application:start(inets),
-  d2api:set_api_key("api_key"),
   application:start(d2api).
 
 start(_StartType, _StartArgs) ->
+  case application:get_env(api_key_file) of
+    {ok, File} ->
+      {ok, BinKey} = file:read_file(filename:absname(File)),
+      Key = binary_to_list(BinKey),
+      d2api:set_api_key(Key),
+      error_logger:info_report([{"api key", Key}]);
+    _ ->
+      ok
+  end,
   d2api_sup:start_link().
 
 stop(_State) ->
